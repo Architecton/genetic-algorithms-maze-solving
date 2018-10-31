14661e4a460615d8e5d2a98b0eb6b34173f5d89d
Jernej Vivod <je.vivod@gmail.com>
2018-10-31 20:03:08 +0100

    Continue work on assignment

--- assignment1.R
+++ assignment1.R
@@ -1,5 +1,7 @@
 library(plyr)
+library(rlist)
 
+# Auxiliary functions ###
 printMaze <- function(maze, rows, cols) {
 	for (x in seq(1, rows)) {
 		print(maze[((x-1)*cols +1) : (x*cols)])
@@ -41,6 +43,7 @@ moveRight <- function(position, rows, cols) {
         return (position + 1)
     }
 }
+# Auxiliary functions ###
 
 simulateSolution <- function(maze, solution, rows, cols) {
     # Update this function to serve as a fitness funcition
@@ -70,30 +73,46 @@ simulateSolution <- function(maze, solution, rows, cols) {
 	return(0)
 }
 
-# TODO
-# Return new agents that are the result of tournament selection.
+# crossover: perform crossover between breeder1, breeder2 and return the offspring.
+crossover <- function(breeder1, breeder2) {
+	# TODO
+}
+
+# Perform tournament selection on list of agents, perform crossover on selected agents and return offspring.
+# Return new list of agents that are the result of tournament selection.
 tournament <- function(agents, num_groups) {
 	# Split agents into groups.
 	groups <- split(agents, ceiling(seq_along(agents)/num_groups))
+
+	# Select best agent with probability P, second best one with probability P*(1-P),
+	# third best one with probability P*(1-P)^2 and so on.
+	P_best = 0.8;  # Probability of selecting the best agent is 0.8
+	prob_sel <- replicate(length(group), P_best)
+	mult <- replicate(length(group), 1-P_best)^(0:(length(group)-1))
+	prob_sel <- prob_sel * mult
+	Pc = 0.9
 
 	# Select two breeders from each group. With probability Pc (crossover probability), 
 	# replace two worst non-breeders from group with offspring of breeders.
 	for (g_idx = 1:length(groups)) {
 		# Define group to be group of agents in next group.
 		group <- groups[[g_idx]]
-		
 		# Sort agents by their fitness.
+		group <- group[list.order(group, (fitness))]
 
-
-		# Select two breeders. Select best one with probability P, second best one with probability P*(1-P),
-		# third best one with probability P*(1-P)^2 and so on.
-		P_best = 0.8;  # Probability of selecting the best agent is 0.8
-		prob_sel <- replicate(length(group), P_best)
-		mult <- replicate(length(group), 1-P_best)^(0:(length(group)-1))
-		prob_sel <- prob_sel * mult
-		# Select breeders.
-		breeders <- sample(group, size=2, replace=FALSE, prob=prob_sel)
+		# If crossover...
+		if(sample(c(1, 0), size=1, prob=c(Pc, 1-Pc))) {
+			# Select breeders.
+			breeders <- sample(group, size=2, replace=FALSE, prob=prob_sel)
+			# Perform crossover to get offspring.
+			offspring <- crossover(breeders[[1]], breeders[[2]])
+			# Replace worst two agents in group with offspring.
+			group[length(group) - 1] = offspring[[1]]
+			group[length(group)] = offspring[[2]]
+		}
 	}
+	# Join groups into list of agents.
+	# TODO TEST
 }
 
 
@@ -112,34 +131,53 @@ geneticAlgorithm <- function(maze, rows, cols, population_size, fitness_f, min_l
 		agents[[k]] = list(fitness = 0, chromosome = chromosome)
 	}
 
+	# counters: iterations and iterations sequence with equal best value.
+	iter_counter <- 0
+	const_counter <- 0
+	# Initialize index of agent with maximum fitness.
+	max_fitness_all <- -1e-10
+	max_fitness_chromosome <- NULL
+
 	# Run genetic algorithm. Break when any of ending conditions evaluate to TRUE.
-	iter_counter <- 0;
-	const_counter <- 0;
 	repeat {
 		# Increment iteration counter.
 		iter_counter <- iter_counter + 1
-		# Compute fitness values for each agent.
-		for(k in 1:population_size) {
-			agents[[k]]$fitness = fitness_f(maze, rows, cols, agents[[k]]$chromosome)
-		}
 
+		# Initialize maximum fitness and index of agent with maximum fitness with default values.
+		max_fitness <- -1e-10
+		idx_max_fitness <- -1
+
+		# Compute fitness values.
+		for(k in 1:population_size) {
+			fitness_nxt <- fitness_f(maze, rows, cols, agents[[k]]$chromosome)
+			if(fitness_nxt > max_fitness) {  # Compare to current max_fitness.
+				max_fitness <- fitness_nxt
+				idx_max_fitness <- k
+			}
+			agents[[k]]$fitness = fitness_nxt
+		}
+		
+		# If greater than maximum fitness value, reset const_counter counter and assign new previous fitness value.
+		if(max_fitness > max_fitness_all) {
+			const_counter <- 0
+			max_fitness_chromosome <- agents[[idx_max_fitness]]$chromosome  # Save chromosome of current best agent.
+		} else {
+			const_counter <- const_counter + 1  # If no improvement, increment counter of iterations with no improvement.
+		}
+		
 		# Select agents for reproduction. Use tournament selection.
 		# Replace two worst agents (that were not selected for breeding) in each group with offspring.
 		num_groups = 5;
 		new_agents <- tournament(agents, num_groups)
-
-
-		# Create new agents by combining the candidates.
-
-		# Replace old agents with new ones.
 
 		# If reached maximum number of iterations or if best fitness has not changed for max_run iterations, end.
 		if(iter_counter >= lim_run || const_counter >= max_run) {
 			break;
 		}
 	}
-	return(agents)
-	
+
+	# Return gene of agent with maximum fitness.
+	return (max_fitness_chromosome)
 }
 
 # fitness_f: fitness function for maze solution. 
